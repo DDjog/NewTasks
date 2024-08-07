143ea8e6aa03eb93e16e0b10507aad4b8df2bc9e
DDjog <dorota_drozdz@o2.pl>
2024-08-07 21:37:05 +0200

    added test

--- SortedListOfNumbers.py
+++ SortedListOfNumbers.py
@@ -4,6 +4,26 @@ numbers = [6,4,2,1,3,2,1,6]
 
 
 def sorting_numbers(numbers):
+    """
+            Sorting list of numbers
+
+            Parameters
+            ----------
+            numbers: integer
+               the list of numbers
+
+            Returns
+            -------
+            numbers: integer
+                the sorted list of numbers
+
+            Examples
+            --------
+            Sorting list of numbers
+            rslt=sorting_numbers(6,4,2,1,3,2,1,6)
+            print(rslt)
+            (1,1,2,2,3,4,6,6)
+            """
     n = len(numbers)
     for i in range(n):
         for j in range (0, n-i-1):
