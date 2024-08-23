f1e334a4501e1ebdf53c71fa1d9b93364e6bf618
DDjog <dorota_drozdz@o2.pl>
2024-08-23 21:26:42 +0200

    added docstings

--- SortingByCounting.py
+++ SortingByCounting.py
@@ -5,6 +5,31 @@ DS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 def sorting_by_counting(OR, H, DS):
+    """
+                   Sorting numbers by counting
+
+                   Parameters
+                   ----------
+                   OR, H, DS: lists of integers
+                      OR list of original numbers
+                      H list of counts of specific number
+                      DS list of sorted numbers
+
+                   Returns
+                   -------
+                   list of integers
+                       the sorted list of numbers
+
+                   Examples
+                   --------
+                   Sorting numbers by counting
+                   OR = [3, 5, 4, 8, 1, 3, 9, 7, 4, 7]
+                   H = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                   DS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                   rslt=sorting_by_counting(OR, H, DS)
+                   print(rslt)
+                   [1,3,3,4,4,5,7,7,8,9]
+                   """
     try:
         if all(value > 0 for value in OR):
             for idx in range(len(OR)):
