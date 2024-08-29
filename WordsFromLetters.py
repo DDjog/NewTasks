d6b3837edcfca52d47bc77474aef77acbfc99c99
DDjog <dorota_drozdz@o2.pl>
2024-08-29 21:56:31 +0200

    Added possibility of creating 3-4 letters long words and docstrings

--- WordsFromLetters.py
+++ WordsFromLetters.py
@@ -1,42 +1,83 @@
-import enchant
+import hunspell
+# import enchant
 from itertools import permutations
 letters = ['a', 'c', 'e', 'i', 'l', 's', 'y', 'z', 'z']
-d = enchant.Dict("en_US")
-
-
-def find_words(d, letters):
-    words_list = set()
-
-    for length in range (2, len(letters)+1):
-        for perm in permutations(letters, length):
-            word = "".join(perm)
-            if d.check(word):
-                words_list.add(word)
-
-    return words_list
-
-
-rslt = find_words(d, letters)
-print(rslt)
-
-
-
-
-# def find_word(d, letters):
-#     words_list = []
-#     letters_copy = letters.copy()
+aff_file = '/User/Dorota/Downloads/pl-dict.aff'
+dic_file = '/User/Dorota/Downloads/pl-dict.dic'
 #
-#     for word in d:
-#         for l in word:
-#             if l in letters_copy:
-#                 letters_copy.remove(l)
-#             else:
-#                 break
-#         else:
-#             words_list.append(word)
+hspell = hunspell.HunSpell(dic_file, aff_file)
+# d = enchant.Dict("pl_PL")
 #
-#     return words_list
+# def find_words(hspell, letters):
+"""
+               Finding words from letters
+
+               Parameters
+               ----------
+               args: ?, list
+                     ?, list of letters
+
+               Returns
+               -------
+               set of words and set of words with the length of 3 or 4 letters
+
+               Examples
+               --------
+               Finding words from letters
+               letters = ['a', 'c', 'e', 'i', 'l', 's', 'y', 'z', 'z']
+               rslt=find_words(hspell, letters)
+               print(rslt)
+               ??
+               """
+#     words_list = set()
+#     words_list_3_4 = set()
 #
-# 
-# rslt = find_word(d, letters)
+#     for length in range (2, len(letters)+1):
+#         for perm in permutations(letters, length):
+#             word = "".join(perm)
+#             if hspell.check(word):
+#                 words_list.add(word)
+#                 if length == 3 or length ==4:
+#                     words_list_3_4.add(word)
+#
+#     return words_list, words_list_3_4
+#
+#
+# rslt = find_words(hspell, letters)
 # print(rslt)
+
+import os
+
+
+hunspell_dirs = [
+    "/usr/share/hunspell",
+    "/usr/local/share/hunspell",
+    "/Library/Spelling",
+    "C://Program Files//Hunspell//Dictionaries"
+]
+
+
+available_languages = []
+
+
+for directory in hunspell_dirs:
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            if filename.endswith(".dic"):
+                language = filename.split('.')[0]
+                available_languages.append(language)
+
+
+print("Dostępne słowniki hunspell:")
+for language in available_languages:
+    print(language)
+
+
+if 'pl_PL' in available_languages:
+    print("\nSłownik polski (pl_PL) jest dostępny.")
+else:
+    print("\nSłownik polski (pl_PL) nie jest dostępny.")
+
+print(hspell.spell("przykład"))
+
+
